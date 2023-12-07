d50e8b5f8736c6b68495d0cd3c69bb3733abadae
Komornikx <68018904+Komornikx@users.noreply.github.com>
2023-12-07 12:41:08 +0100

    Database connection module init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose';
+
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/stah-swipe-nest'),
-  ],
+  imports: [DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
